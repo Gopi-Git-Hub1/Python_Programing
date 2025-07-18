5131e81f788059b5a0c08244eb0d7a2505831a79
Chappidi Gopi <chappidi.gopi@capgemini.com>
2025-07-18 12:28:12 +0000

    keyword repo changed to keywors python program

---
+++ keywords/keywords.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+print ('Python keywords..')
+# python program single line comment
+# python keywords 
+''' python
+    program
+    multiline 
+    comments '''
+print ("""
+          and	  as	 assert
+          break	  class	 continue
+          def	  del	 elif
+          else    except False
+          finally for	 from
+          global  if	 import
+          in	  is	 lambda
+          None	 nonlocal 	not
+          or	 pass	 raise
+          return True	 try
+          while	 with	 yield""")
+""" python
+    program
+    multiline 
+    comments """
